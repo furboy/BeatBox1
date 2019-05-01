018474fa900082e207365b2301d0da4cd6e9085e
furboy <46965862+furboy@users.noreply.github.com>
2019-05-01 08:50:39 -0500

    Completed day 3

---
+++ app/src/main/java/com/example/beatbox/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.beatbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ctech.aleco.beatbox.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_beat_box)
+    }
+}
