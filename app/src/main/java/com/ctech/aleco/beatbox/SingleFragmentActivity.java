8a4be4e44693d81a6dbdbfb75d844358405a774b
Mr. Bartucz <jobartucz@isd535.org>
2019-05-02 09:34:56 -0500

    line 15: supposed to be setContentView(getLayoutResId());
    line 15: supposed to be setContentView(getLayoutResId());

--- app/src/main/java/com/ctech/aleco/beatbox/SingleFragmentActivity.java
+++ app/src/main/java/com/ctech/aleco/beatbox/SingleFragmentActivity.java
@@ -12,7 +12,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_beat_box);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
